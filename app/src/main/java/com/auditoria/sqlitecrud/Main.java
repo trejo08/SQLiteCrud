ba7e74dba074fc5a681fa64433225d51b3039f40
Juan Trejo <juan@gr33nmedia.com>
2014-11-05 13:16:55 -0600

    adding activity add person view

--- app/src/main/java/com/auditoria/sqlitecrud/Main.java
+++ app/src/main/java/com/auditoria/sqlitecrud/Main.java
@@ -36,9 +36,6 @@ public class Main extends Activity {
                 startActivity(goToPerson);
             }
         });
-
-
-
     }
 
 
--- app/src/main/java/com/auditoria/sqlitecrud/ManagePersonView.java
+++ app/src/main/java/com/auditoria/sqlitecrud/ManagePersonView.java
@@ -1,17 +1,28 @@
 package com.auditoria.sqlitecrud;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class ManagePersonView extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_person_view);
+
+        Button addPerson = (Button) findViewById(R.id.btn_add_person);
+        addPerson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent showAddPersonView = new Intent(getApplicationContext(), AddPersonView.class);
+                startActivity(showAddPersonView);
+            }
+        });
     }
 
 
